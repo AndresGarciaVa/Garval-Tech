88949e79ee9fe3bae98638853490828d79c61d4b
AndresGarciaVa <andresgarciav901@gmail.com>
2023-10-24 17:00:01 -0500

    modified:   src/App.jsx 	modified:   src/components/sidebar/links/Links.jsx 	modified:   src/components/sidebar/toggleButton/ToggleButton.jsx

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,7 +1,15 @@
 const Links = () => {
-  return (
-    <div className="Link">Links</div>
-  )
-}
+  const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
 
-export default Links
+  return (
+    <div className="links">
+      {items.map( (item) => (
+        <a href={`#${item}`} key={item}>
+          {item}
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export default Links;
--- src/components/sidebar/toggleButton/ToggleButton.jsx
+++ src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -1,10 +1,10 @@
 
-const ToggleButton = (setOpen) => {
+const ToggleButton = ({setOpen}) => {
     return (
       <button onClick={() => setOpen((prev) => !prev)}>Button
           
       </button>
-    )
-  }
+    );
+  };
   
   export default ToggleButton
