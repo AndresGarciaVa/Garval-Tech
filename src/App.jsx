76dafc28781b656d7023b22472aacd196db4b02f
AndresGarciaVa <andresgarciav901@gmail.com>
2023-10-25 10:16:02 -0500

    modified:   src/App.jsx 	new file:   src/components/hero/Hero.jsx 	new file:   src/components/hero/hero.scss 	modified:   src/components/sidebar/Sidebar.jsx 	modified:   src/components/sidebar/links/Links.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import Test from "./Test";
 import "./app.scss";
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 
 
@@ -7,6 +8,7 @@ const App = () => {
   return <div>
     <section id="Home">
       <Navbar/>
+      <Hero/>
     </section>
     <section id="Servicios">Parallax</section>
     <section>Services</section>
