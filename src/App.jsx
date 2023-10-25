335daee22234d81ef88d108143088d074c29e70f
AndresGarciaVa <andresgarciav901@gmail.com>
2023-10-25 11:53:50 -0500

    modified:   src/App.jsx 	new file:   src/components/parallax/Parallax.jsx 	new file:   src/components/parallax/parallax.scss 	new file:   src/components/services/Services.jsx 	new file:   src/components/services/services.scss 	modified:   src/components/sidebar/sidebar.scss

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,32 @@
 import "./app.scss";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
-
+import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
 
 const App = () => {
-  return <div>
-    <section id="Home">
-      <Navbar/>
-      <Hero/>
-    </section>
-    <section id="Servicios">Parallax</section>
-    <section>Services</section>
-    <section id="Portfolio">Parallax</section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
-    <section id="Contacto">Contact</section>
-    {/* <Test/> */}
-
-  </div>;
+  return (
+    <div>
+      <section id="Home">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Servicios">
+        <Parallax type="servicios" />
+      </section>
+      <section>
+        <Services/>
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id="Contacto">Contact</section>
+      {/* <Test/> */}
+    </div>
+  );
 };
 
 export default App;
