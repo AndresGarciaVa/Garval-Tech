1086dce097f2927d8b7388ec0f1bc4989eaf74ca
AndresGarciaVa <andresgarciav901@gmail.com>
2023-10-27 10:32:20 -0500

    modified:   src/components/services/Services.jsx 	modified:   src/components/services/services.scss

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,13 +1,119 @@
-import "./services.scss"
+import "./services.scss";
+import { animate, motion, useInView } from "framer-motion";
+
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 const Services = () => {
-  return (
-    <div className="services">
-        <div className="textContainer"></div>
-        <div className="titleContainer"></div>
-        <div className="listContainer"></div>
-    </div>
-  )
-}
 
-export default Services
+  const ref = useRef()
+
+
+  const isInView = useInView(ref,{margin:"-100px"})
+
+  return (
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      // animate="animate"
+      // whileInView="animate"
+      ref={ref}
+    >
+      <motion.div className="textContainer" variants={variants}>
+        <p>
+          Somos tu aliado estratégico para impulsar el crecimiento y <br />
+          el desarrollo de tu marca o idea.
+        </p>
+        <hr />
+      </motion.div>
+      <motion.iv className="titleContainer" variants={variants}>
+        <div className="title">
+          <img src="./people.webp" alt="" />
+          <h1>
+            <b>Ideas</b> Únicas.
+          </h1>
+        </div>
+        <div className="title">
+          <h1>
+            <b>Innovación </b>Para Ti
+          </h1>
+          <button>QUE HACEMOS?</button>
+        </div>
+      </motion.iv>
+      <motion.div className="listContainer" variants={variants}>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam sed
+            deserunt at possimus! Harum ut sed quisquam. A quisquam consequatur
+            tenetur beatae perspiciatis et non. Voluptas dolores rerum ipsa
+            itaque!
+          </p>
+          {/* <button>Ir</button> */}
+        </motion.div>
+
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam sed
+            deserunt at possimus! Harum ut sed quisquam. A quisquam consequatur
+            tenetur beatae perspiciatis et non. Voluptas dolores rerum ipsa
+            itaque!
+          </p>
+          {/* <button>Ir</button> */}
+        </motion.div>
+
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam sed
+            deserunt at possimus! Harum ut sed quisquam. A quisquam consequatur
+            tenetur beatae perspiciatis et non. Voluptas dolores rerum ipsa
+            itaque!
+          </p>
+          {/* <button>Ir</button> */}
+        </motion.div>
+
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam sed
+            deserunt at possimus! Harum ut sed quisquam. A quisquam consequatur
+            tenetur beatae perspiciatis et non. Voluptas dolores rerum ipsa
+            itaque!
+          </p>
+          {/* <button>Ir</button> */}
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Services;
