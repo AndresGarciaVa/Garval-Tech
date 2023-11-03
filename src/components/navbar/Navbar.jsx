2e4fcdb805065f3f2a542b6f67ef55704574a95a
AndresGarciaVa <andresgarciav901@gmail.com>
2023-11-03 15:52:48 -0500

    modified:   src/components/navbar/Navbar.jsx 	modified:   src/components/navbar/navbar.scss 	modified:   src/components/portfolio/portfolio.scss

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const navbar = () => {
             initial={{opacity:0, scale:0.5}}
             animate={{opacity:1, scale:1}}
             transition={{duration:0.5}}>
-              </motion.img>
+            </motion.img>
             <div className="social">
                 <a href="https://www.facebook.com/profile.php?id=61553253624756"><img src="/facebook.png" alt="Facebook" /></a>
                 <a href="https://instagram.com/garval.tecnologies?igshid=OGQ5ZDc2ODk2ZA=="><img src="/instagram.png" alt="Instagram"/></a>
