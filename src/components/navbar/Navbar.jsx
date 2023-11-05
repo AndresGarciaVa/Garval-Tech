03abff565d639557b3a3e83379198e09b6373290
AndresGarciaVa <andresgarciav901@gmail.com>
2023-11-05 11:23:03 -0500

    modified:   src/components/navbar/Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const navbar = () => {
       <div className="wrapper">
         <motion.img
           className="img3"
-          src="./public/logo.png"
+          src=".\public\logo.png"
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
